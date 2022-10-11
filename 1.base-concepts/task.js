dc346cb60a0024920ddacd2864102ca000770d41
Richard <Artyouha@yandex.ru>
2022-10-11 22:37:07 +0200

    com

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,25 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = {};
+  let D = b**2 - 4 * a * c;
+    if(D < 0)
+        return false;
+    arr['Дискриминант = '] = D;
+    if(D == 0)
+        arr["Корень равен"] = -b  / (2 * a);
+    else if(D > 0){
+        let tmp = [];
+        tmp.push((-b + Math.sqrt(D)) / (2 * a));
+        tmp.push((-b - Math.sqrt(D)) / (2 * a));
+        arr["Корень равен"] = tmp;
+    }
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
+  let totalAmount = {};
+let S = amount - contribution;
+let month = (amount * percent) / ( 1 - ( 1 / ( ( 1 + interestRate ) **date ) ) );
+let P = percent/12/100;
+let summ = S * (P + (P / (((1 + P)^month) - 1)))
   return totalAmount;
 }
